cae9b4ec7c7911105a98b99e00d3758f14d261bf
Derick Vigne <derickvigne@me.com>
2019-03-20 21:07:06 -0400

    Finished Huffman Encoding

--- huffman.py
+++ huffman.py
@@ -1,18 +1,94 @@
 import numpy as np
+import heapq
 
-arr = np.array([.07, .2, .19, .36, .18])
-encoded = np.zeros_like(arr, dtype="int64")
-openFile = open("test.txt", "w+")
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
 
-def doEncoding(arr, fileObj, curr, pos):
-    if(max(arr) == 0):
-        return
-    if(arr[pos] == max(arr)):
-        encoded[pos] = curr
-        arr[pos] = 0
-        doEncoding(arr, fileObj, curr = curr + 1, pos = 0)
-    doEncoding(arr, fileObj, curr = curr, pos = pos + 1)
+    def __lt__(self, node):
+        if(not node):
+            raise ValueError("Node value of NoneType cannot be compared")
+        return node.value > self.value
 
-doEncoding(arr, openFile, 0, 0)
-for x in encoded:
-    openFile.writelines(bin(x)[2:] + "\n")
+
+class HuffmanTree:
+    def __init__(self, fileObject):
+        self.nodes = []
+        self.file = fileObject
+
+    def insert(self, value):
+        newNode = Node(value)
+        heapq.heappush(self.nodes, newNode)
+        return newNode
+
+    def doHuffman(self):
+        while(len(self.nodes) > 1):
+            node2, node1 = heapq.heappop(self.nodes), heapq.heappop(self.nodes)
+            newNode = self.insert(node1.value + node2.value)
+            newNode.right, newNode.left = node1, node2
+
+    def traverse(self, node, depth = 0):
+        if(not node):
+            return
+        self.traverse(node.left, depth = depth + 1)
+        print("Depth: %s - %s" % (depth, node.value))
+        self.traverse(node.right, depth = depth + 1)
+
+    def traverse2(self, node, direction):
+        if(not node):
+            return
+        print(node.value)
+        if(direction):
+            self.traverse2(node.left, direction)
+        else:
+            self.traverse2(node.right, direction)
+
+
+    def printHeap(self):
+        print(self.nodes)
+
+    def isBst(self, node, max, min):
+        if(not node):
+            return True
+        if (node.value < min or node.value > max):
+            return False
+        return self.isBst(node.left, min, node.value - 1) and self.isBst(node.right, node.value + 1, max)
+
+    def getRoot(self):
+        return self.nodes[0]
+
+    def generateHuffmanCode(self, value, node = None, hCode = ""):
+        if(not node):
+            return
+        self.generateHuffmanCode(value, node.left, hCode = hCode + "1")
+        if(node.value == value):
+            self.file.write("%s\t\t%s\n" % (value, hCode))
+        self.generateHuffmanCode(value, node.right, hCode = hCode + "0")
+
+inFile = open("probabilities.txt", "r")
+outFile = open("codes.txt", "w")
+outFile.write("probability\tcode\n\n")
+arr = []
+
+for line in inFile:
+    arr.append(float(line))
+
+print("Number if lines in file: %s" % len(arr))
+
+inFile.seek(0)
+
+heapq.heapify(arr)
+huffmanTree = HuffmanTree(outFile)
+print(arr)
+
+for x in range(0, len(arr)):
+    huffmanTree.insert(arr[x])
+
+huffmanTree.doHuffman()
+
+for line in inFile:
+    huffmanTree.generateHuffmanCode(float(line), huffmanTree.getRoot())
+
+outFile.close()
